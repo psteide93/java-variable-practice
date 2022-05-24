8228e8f33ac7ef6038b0843a1a58cc5ad5e1ab71
Parnell <psteide@ford.com>
2022-05-24 08:20:54 -0400

    Add variables

--- App.java
+++ App.java
@@ -1,6 +1,20 @@
 class App {
   public static void main(String args[]){
-    String message = "Hello, world!";
-    System.out.println(message);
+    String vin = "1_258_874";
+    String vehicleMake = "Ford";
+    String vehicleModel = "F-150";
+    String vehicleColor = "blue";
+    boolean towingPackage = true;
+    int odom = 157_845;
+    int price = 10_000;
+    char quality = 'C'; 
+
+    System.out.println(vin);
+    System.out.println(String.format("The make of my %s vehcie is %s and it's a %s",vehicleColor,vehicleMake,vehicleModel));
+    System.out.println(odom);
+    System.out.println(price);
+    System.out.println(String.format("The quality of my vehicle is %c",quality));
+    System.out.println(towingPackage);
+    
   }
 }
